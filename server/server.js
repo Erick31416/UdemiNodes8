af3b6411ec5aa0f93ca70cd18027e90c8065155e
Erick31416 <martin.aranda.ricardo@gmail.com>
2020-04-01 19:49:02 +0200

    primer comit

---
+++ server/server.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+const bodyParser = require('body-parser');
+//si veo app use es un midelware
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/usuario', function (req, res) {
+  res.json('getUsuario');
+});
+app.post('/usuario', function (req, res) {
+  let body = req.body;
+  if (body.nombre === undefined) {
+    res.status(400).json({
+      ok: false,
+      mensaje: 'El nombre es necesario'
+    });
+  }else{
+    res.json({
+      body
+    });
+  }
+});
+app.put('/usuario/:id', function (req, res) {
+  let identificador = req.params.id; // alkaparra : params no param
+  res.json({
+    identificador
+  });
+});
+app.delete('/usuario', function (req, res) {
+  res.json('delete Usuario');
+});
+app.get('/', function (req, res) {
+  res.json('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
