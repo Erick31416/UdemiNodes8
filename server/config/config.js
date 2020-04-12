f6fdbb864f627edbaa23ecda176c13102b33f3cd
Erick31416 <martin.aranda.ricardo@gmail.com>
2020-04-12 14:23:41 +0200

    creo que ya

--- server/config/config.js
+++ server/config/config.js
@@ -20,13 +20,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev'; //NODE_ENV LO CREA HEROKU
 // contraseña y usuario del admin de la db
 //urlDb = 'mongodb+srv://santxiki:Sakaneta_22_151113@cluster0-jst72.mongodb.net/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
 
-if (process.env.NODE_ENV === 'dev' && '1' == '2'){
+if (process.env.NODE_ENV === 'dev'){
     urlDb = 'mongodb://localhost:27017/cafe';
 }else{
 
     urlDb = process.env.MONGO_URI;
-    console.log({urlDb});
-    urlDb = 'mongodb+srv://nodeUser:zhqhqZiNBTnsx0t9@cluster0-jst72.mongodb.net/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
+
 }
 
 process.env.URL_DB = urlDb;
