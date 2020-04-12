2cc9d7f6460042b2b6e77534ae14ee57ceab975c
Erick31416 <martin.aranda.ricardo@gmail.com>
2020-04-12 13:49:09 +0200

    varialbe entorno

--- server/config/config.js
+++ server/config/config.js
@@ -20,10 +20,13 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev'; //NODE_ENV LO CREA HEROKU
 // contraseña y usuario del admin de la db
 //urlDb = 'mongodb+srv://santxiki:Sakaneta_22_151113@cluster0-jst72.mongodb.net/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
 
-if (process.env.NODE_ENV === 'dev'){
+if (process.env.NODE_ENV === 'dev' && '1' == '2'){
     urlDb = 'mongodb://localhost:27017/cafe';
 }else{
-    urlDb = 'mongodb+srv://nodeUser:zhqhqZiNBTnsx0t9@cluster0-jst72.mongodb.net/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
+
+    urlDb = process.env.urlDb;
+    //console.log({urlDb});
+    //urlDb = 'mongodb+srv://nodeUser:zhqhqZiNBTnsx0t9@cluster0-jst72.mongodb.net/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
 }
 
 process.env.URL_DB = urlDb;
